4232bbcf5ffe3c577844fc894a4eaab28d0b01e3
AmyCong <578375250@qq.com>
2024-06-03 10:18:06 +0800

    base

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const assets = document.querySelectorAll('.asset');
+    assets.forEach(asset => {
+        asset.addEventListener('click', () => {
+            alert('Asset clicked!');
+        });
+    });
+});
