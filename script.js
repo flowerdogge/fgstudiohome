6c5f7a1c1244fe0c72e136510793273a21ea7c29
AmyCong <578375250@qq.com>
2024-06-03 12:41:44 +0800

    增加购买链接URL

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const assets = document.querySelectorAll('.asset');
     assets.forEach(asset => {
         asset.addEventListener('click', () => {
-            alert('Asset clicked!');
+        //    alert('Asset clicked!');
         });
     });
 });
